6d74f2bbec8201f8286522af2395b0883c6823bc
whenofficial <fsmitchm@gmail.com>
2018-07-04 22:29:33 +0400

    Make main component

--- src/src/App.js
+++ src/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Titlebar from './components/Titlebar';
+import Main from './views/Main';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Titlebar />
+        <Main />
       </div>
     );
   }
---
+++ src/src/views/Main.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './assets/Main.css';
+
+class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div className="app">
+                :)
+            </div>
+         )
+    }
+}
+ 
+export default Main;
